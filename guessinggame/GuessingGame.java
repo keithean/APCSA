986da3841e3d42e57b836ae94e7c7e5c1291da14
keithean <134558542+keithean@users.noreply.github.com>
2023-12-14 09:37:35 +0000

    Deleted the GuessingGame.class file (not sure why I made it) and finished the Guessing Game

--- guessinggame/GuessingGame.java
+++ guessinggame/GuessingGame.java
@@ -1,5 +1,28 @@
-class GuessingGame {
-    public static void main(String[] args) {
-        System.out.println("Hello!");
+import java.util.Scanner;
+
+public class GuessingGame {
+ public static void main(String[] args)
+    {
+       Scanner input = new Scanner(System.in);
+        int secretNumber = (int)(Math.random() * 100) + 1;
+        int guess = 0;
+        int guessCount = 0;
+        System.out.println("Guess my number!");
+        while (guess != secretNumber){
+            System.out.println("Pick a number between 1 and 100");
+            guess = input.nextInt();
+    
+             if (guess < secretNumber) {
+                System.out.println("The number you guessed is too low, try again");
+                guessCount += 1;
+            } else if (guess > secretNumber) {
+                System.out.println("The number you guessed is too high, try again");
+                guessCount += 1;
+            } else {
+                System.out.println("Congrats! You guessed the correct number of " + secretNumber);
+                guessCount += 1;
+                System.out.println("It took you " + guessCount + " guesses");
+            }
+       }
     }
 }
